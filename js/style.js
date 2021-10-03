39fbe4eaae2bbad9753da8ad83edf91ceb74bc63
rabonas <86463452+rabonas@users.noreply.github.com>
2021-10-03 21:11:37 +0500

    my fifth commit

--- js/style.js
+++ js/style.js
@@ -42,3 +42,32 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
 });
+
+// const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
+//   Dots: false,
+//   on: {
+//     createSlide: (carousel, slide) => {
+//       slide.Panzoom = new Panzoom(slide.$el.querySelector(".panzoom"), {
+//         panOnlyZoomed: true,
+//         resizeParent: true,
+//       });
+//     },
+//     deleteSlide: (carousel, slide) => {
+//       if (slide.Panzoom) {
+//         slide.Panzoom.destroy();
+//         slide.Panzoom = null;
+//       }
+//     },
+//   },
+// });
+
+// const thumbCarousel = new Carousel(document.querySelector("#thumbCarousel"), {
+//   Sync: {
+//     target: mainCarousel,
+//     friction: 0,
+//   },
+//   Dots: false,
+//   Navigation: false,
+//   center: true,
+//   infinite: false,
+// });
